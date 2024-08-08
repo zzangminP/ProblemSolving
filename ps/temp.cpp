563b9cab9cc3b8f7ce79c21ac3ffc5f2cebfe3a3
zzangmin <wjdals1212gh@gmail.com>
2024-08-09 00:07:42 +0900

    [Solving] 7576
    https://www.acmicpc.net/problem/7576

--- ps/temp.cpp
+++ ps/temp.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int max = 0;
+    int index;
+    int a[9];
+
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> a[i];
+        if (a[i] > max)
+        {
+            max = a[i];
+            index = i + 1;
+        }
+    }
+    cout << max << "\n" << index;
 }
