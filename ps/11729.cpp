e4c4f10af5953838fe8d1dd06fe5da0932d6a1e6
zzangmin <wjdals1212gh@gmail.com>
2024-12-23 20:54:21 +0900

    [Solved] 11719 recursion
    https://www.acmicpc.net/problem/4179

---
+++ ps/11729.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void hanoi(int a, int b, int n)
+{
+    if (n == 1)
+    {
+        cout << a << ' ' << b << '\n';
+        return;
+    }
+
+    hanoi(a, 6 - a - b, n - 1);
+    cout << a << ' ' << b << '\n';
+    hanoi(6 - a - b, b, n - 1);
+
+}
+
+
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n = 0;
+    cin >> n;
+    cout << (1 << n) - 1 << '\n';
+    hanoi(1, 3, n);
+
+}
